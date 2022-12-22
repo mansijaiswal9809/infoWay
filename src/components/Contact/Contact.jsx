e46067818871d8d9737748c91835b048536e6e51
Atulpagrut <atulpagrut007@gmail.com>
2022-12-22 10:05:06 +0530

    Contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,58 +3,68 @@ import './Contcact.css'
 const Contact = () => {
   return (
     <div className='container'>
-      <div className="top_container"><img src="https://www.beekeeper.io/wp-content/uploads/2021/02/Manufacturing-Trends-FI.jpg" alt="" /></div>
+      <div className="top_container"><img src="https://www.beekeeper.io/wp-content/uploads/2021/02/Manufacturing-Trends-FI.jpg"  style={{width:"80%"}} alt="" /></div>
       <div className="bottom_container">
         <div className="left_cotainer">
           <div className="mapouter">
-          <div className='text_item'>
+            <div className='text_item'>
               <h1>USA</h1>
               <p>Address:46520 Fremont Blvd,Suite 614 Fremont CA – 94538.Phone:+631-923-8350e-mail: info@infowaygroup.com</p>
-              </div>
+            </div>
             <div className="gmap_canvas">
-              <iframe src="https://maps.google.com/maps?q=46520%20Fremont%20Blvd,%20Suite%20614%20Fremont%20CA%20%E2%80%93%2094538.&amp;t=k&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" id="gmap_canvas_1" frameborder="0" scrolling="no" style={{ width: "600px", height: "400px" }}></iframe>
-              <a href="https://technologg.com/how-to-change-screen-orientation-in-windows-11/" style={{ display: 'none' }}>How to Change Screen Orientation in Windows 11</a>
+              <iframe src="https://maps.google.com/maps?q=46520%20Fremont%20Blvd,%20Suite%20614%20Fremont%20CA%20%E2%80%93%2094538.&amp;t=k&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" id="gmap_canvas_1" frameborder="0" scrolling="no" style={{ width: "400px", height: "300px" }}></iframe>
+              <a href="https://technologg.com/how-to-change-screen-orientation-in-windows-11/" style={{ display: 'none' }}></a>
             </div>
           </div>
           <div className="mapouter">
             <div className='text_item'>
-            <h1>LOS ANGELES OFFICE</h1>
-              <p>Address: 18766, Pioneer Blvd,Suite 614 Artesia, CA – 91326.Phone: +925-640-6720e-mail: LA@infowaygroup.com</p>
+              <h1>LOS ANGELES OFFICE</h1>
+              <h4>Address: 18766, Pioneer Blvd,Suite 614 Artesia, CA – 91326.Phone: +925-640-6720</h4><br />
+              <h4>E-mail: LA@infowaygroup.com</h4>
             </div>
-            <div className="gmap_canvas">              
-              <iframe src="https://maps.google.com/maps?q=18764-18766%20Pioneer%20Blvd%20,%20Artesia,%20CA%2090701,%20USA&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" id="gmap_canvas_2" frameborder="0" scrolling="no" style={{ width: "600px", height: "400px" }}></iframe>
-              <a href="https://technologg.com/how-to-change-screen-orientation-in-windows-11/" style={{ display: "none" }}>How to Change Screen Orientation in Windows 11</a>
-              </div>
+            <div className="gmap_canvas">
+              <iframe src="https://maps.google.com/maps?q=18764-18766%20Pioneer%20Blvd%20,%20Artesia,%20CA%2090701,%20USA&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" id="gmap_canvas_2" frameborder="0" scrolling="no" style={{ width: "400px", height: "300px" }}></iframe>
+              <a href="https://technologg.com/how-to-change-screen-orientation-in-windows-11/" style={{ display: "none" }}></a>
             </div>
+          </div>
           <div className="mapouter">
             <div className='text_item'>
-            <h1>PUDUCHERRY OFFICE</h1>
+              <h1>PUDUCHERRY OFFICE</h1>
               <p>Address:40,First Floor,Rajiv Gandhi Street,Senthamarai Nagar,Muthialpet ,Puducherry - 605003.Phone: +91-8056655291e-mail: puducherry@infowaygroup.com</p>
             </div>
-            <div className="gmap_canvas">              
-              <iframe src="https://maps.google.com/maps?q=8/17%20,%20Second%20Floor,%20Tambaram-Velacherry%20Main%20Road,%20Vijayanagaram,Medavakkam,%20Chennai%20-%20600100.&amp;t=k&amp;z=9&amp;ie=UTF8&amp;iwloc=&amp;output=embed" id="gmap_canvas_3" frameborder="0" scrolling="no" style={{ width: '600px', height: '660px' }}></iframe>
+            <div className="gmap_canvas">
+              <iframe src="https://maps.google.com/maps?q=8/17%20,%20Second%20Floor,%20Tambaram-Velacherry%20Main%20Road,%20Vijayanagaram,Medavakkam,%20Chennai%20-%20600100.&amp;t=k&amp;z=9&amp;ie=UTF8&amp;iwloc=&amp;output=embed" id="gmap_canvas_3" frameborder="0" scrolling="no" style={{ width: '400px', height: '300px' }}></iframe>
               <a href="https://technologg.com/how-to-change-screen-orientation-in-windows-11/" style={{ display: "none" }}>How to Change Screen Orientation in Windows 11</a>
-              </div>
             </div>
+          </div>
           <div className="mapouter">
             <div className='text_item'>
-            <h1>CHENNAI OFFICE</h1>
+              <h1>CHENNAI OFFICE</h1>
               <p>Address: 8/17 , Second Floor,Tambaram-Velacherry Main Road,Vijayanagaram,Medavakkam,Chennai - 600100.Phone: +91-9500505590e-mail: chennai@infowaygroup.com</p>
             </div>
-            <div className="gmap_canvas">              
-              <iframe src="https://maps.google.com/maps?q=7030%20Woodbine%20Avenue%20,%20Suite%20500%20,%20Markham%20Ontario%20L3R%206G2%20,%20Canada&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" id="gmap_canvas_4" frameborder="0" scrolling="no" style={{ width: "600px", height: "400px" }}></iframe>
+            <div className="gmap_canvas">
+              <iframe src="https://maps.google.com/maps?q=7030%20Woodbine%20Avenue%20,%20Suite%20500%20,%20Markham%20Ontario%20L3R%206G2%20,%20Canada&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" id="gmap_canvas_4" frameborder="0" scrolling="no" style={{ width: "400px", height: "300px" }}></iframe>
               <a href="https://technologg.com/how-to-change-screen-orientation-in-windows-11/" style={{ display: "none" }}>How to Change Screen Orientation in Windows 11</a>
             </div>
+          </div>
+          <div className="mapouter">
+            <div className='text_item'>
+              <h1>CANADA OFFICE</h1>
+              <p>Address: 7030 Woodbine Avenue ,Suite 500 , Markham Ontario L3R 6G2 , Canada.e-mail: canada@infowaygroup.com</p></div>
+            <div className="gmap_canvas">
+              <iframe src="https://maps.google.com/maps?q=160%20Robinson%20Road,%20Singapore%20Business%20Federation%20Centre,%20Singapore%20-%20068914%20&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" id="gmap_canvas_5" frameborder="0" scrolling="no" style={{ width: "400px", height: "300px" }}></iframe>
+              <a href="https://technologg.com/how-to-change-screen-orientation-in-windows-11/" style={{ display: "none" }}>How to Change Screen Orientation in Windows 11</a>
             </div>
-          <div className="mapouter"> 
+          </div>
+          <div class="mapouter">
           <div className='text_item'>
-          <h1>CANADA OFFICE</h1>
-            <p>Address: 7030 Woodbine Avenue ,Suite 500 , Markham Ontario L3R 6G2 , Canada.e-mail: canada@infowaygroup.com</p></div>           
-            <div className="gmap_canvas">            
-             <iframe src="https://maps.google.com/maps?q=160%20Robinson%20Road,%20Singapore%20Business%20Federation%20Centre,%20Singapore%20-%20068914%20&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" id="gmap_canvas_5" frameborder="0" scrolling="no" style={{ width: "600px", height: "400px" }}></iframe>
-             <a href="https://technologg.com/how-to-change-screen-orientation-in-windows-11/" style={{ display: "none" }}>How to Change Screen Orientation in Windows 11</a>
+              <h1>AKOLA OFFICE</h1>
+              <p>Address:Info Way Solutions LLC vitthal Nagar Mothi umari infront of Gajanan Maharaj Temple Akola Email:amol4095@gmail.com</p></div>            
+            <div class="gmap_canvas">
+              <iframe src="https://maps.google.com/maps?q=Vitthal%20nagar,%20Mothi%20umari%20,%20Akola,&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" id="gmap_canvas_6" frameborder="0" scrolling="no" style={{ width: "400px", height: "300px" }}></iframe>
+              <a href="https://technologg.com/best-saas-instagram-accounts/" style={{ display: "none" }}>Best Saas Instagram Accounts</a>
             </div>
-            </div>
+          </div>
         </div>
 
 
