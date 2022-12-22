d7e32f81a60e3dcfa73f0e16756cc61f65bf51cc
Atulpagrut <atulpagrut007@gmail.com>
2022-12-23 01:00:11 +0530

    retail manfa life done

--- src/components/AllIndustries/Retail.jsx
+++ src/components/AllIndustries/Retail.jsx
@@ -3,7 +3,30 @@ import React from 'react'
 const Retail = () => {
   return (
     <div>
-      Retail
+       <h1 style={{color:"black"}}>Retail</h1>
+      <p style={{color:"black"}}>Retail companies operate in environments that are highly dynamic with new challenges and opportunities emerging constantly. Retailers need to provide their customers with new ways to interact and shop, while at the same time meeting the abiding challenges of the business: winning consumers while driving down costs.</p>
+      <p style={{color:"black"}}></p>
+      <ul>
+        <li style={{color:"black"}}>Multi-channel Retailing - The more your consumers see you, the more they will want to know about you. Online consumers will buy if they have seen it, liked it, and are convinced of it. A true multi-channel strategy covers purchases from a store, a website, telephone ordering, mail orders, interactive television, catalog ordering and comparison shopping sites. So, all channels must be closely aligned and monitored for maximum impact and hence, sales.</li>
+        <li style={{color:"black"}}>Mobility in Retail - Consumers around the world are ready to use mobile devices to shop, compare, and transact on the Web or in stores. And there is no greater guarantee of customer loyalty to your operation than making their interaction with you as easy and effective as possible. More and more retailers today are turning to mobile technology at checkout to enhance customer engagement and repeat buyers. What is emerging from this new environment is dramatically enhanced ease of use for shoppers.</li>
+        <li style={{color:"black"}}>Inventory Management - Inventory management is the first challenge that retailers face at the local store level as well as at the warehouse level. Excess inventory often leads to an increase in inventory costs, and therefore retailers need to have IT systems in place for inventory management to reduce costs. IT can help retailers to plan their stock outs, replenish their stock on time, move stock from warehouse to stores, maintain adequate stock at a store to match consumer preferences.</li>
+        <li style={{color:"black"}}>Digital Consumers - Today's consumers have access to information at a click – about product assortment, pricing, quality and everything else there is to know. Given the scenario, companies can create more wallet share by reaching out across digital channels — mobiles, interactive TVs, and social communities.</li>
+        <li style={{color:"black"}}>Virtual Retailing - Virtual Retailing is going to be the future of retailing in the times to come. This provides a good platform where customers are exposed to store items just like in a physical store. Targeting a customer beyond the walls of a physical store is slowly becoming a necessity and a trend. Creating an environment for off-store retailing definitely gives retailers an opportunity to reduce costs in areas where they have been incurring heavy costs such as; assets, setup costs, inventory storage, maintenance, floor lighting, etc., making retailing more profitable for the future. This provides a good platform where customers are exposed to store items just like in a physical store.</li>
+      </ul>
+      <label style={{color:"black"}}>Why Info way solution?</label>
+      <p>Our experts would work in collaboration with you to build solutions that will address your unique challenges.</p>
+      <p>We create smart and inherent shopping experiences by providing services such as:</p>
+      <ul>
+        <li style={{color:"black"}}>Content Management</li>
+        <li style={{color:"black"}}>WAP and Mobile Application Development (for example, Store Locator )</li>
+        <li style={{color:"black"}}>Customized Enterprise Solutions</li>
+        <li style={{color:"black"}}>Medication Order Management System</li>
+        <li style={{color:"black"}}>Regulatory Compliance</li>
+        <li style={{color:"black"}}>Solutions for better compliance with regulatory guidelines</li>
+        <li style={{color:"black"}}>Tracking drug discovery processes and management</li>
+        <li style={{color:"black"}}>Processes for clinical trial project management and tracking</li>
+      </ul>
+      <p style={{color:"black"}}>Our customer-centric approch and our ability to understand your retail business sets us apart from our competitors. </p>
     </div>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      
+    <ul style={{color:"white"}}>
+        Disclaimer
+      </ul>
+    <ul style={{color:"white"}}>
+        Privacy
+      </ul>
+    <ul style={{color:"white"}}>
+        Sitemap
+      </ul>
+    <ul style={{color:"white"}}>
+        Contact Us
+      </ul>
     </div>
   )
 }
